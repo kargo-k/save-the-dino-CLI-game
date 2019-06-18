93cb1d4ae8001239db3d34fe7fa43df3f59fbd80
Philip Sterling <philipssterlings@gmail.com>
2019-06-18 09:52:34 -0700

    added files for classes-database

--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,7 @@
 require_relative '../config/environment'
 
+dic_url = "https://dictionaryapi.com/api/v3/references/collegiate/json/x*?key=fcaaacd2-b734-4cfb-8946-ff1484435c66"
 
 
-i = "HELLO WORLD"
 binding.pry
+0
